beb742e3ff7bed0457bc9447116a4ae2ee949cd1
Midomik <krastikrabbs9234@gmail.com>
2023-08-26 16:11:23 +0300

    comm 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galaryList = document.querySelector(".gallery");
+
+function renderGalery(imgObject) {
+    const renderPhoto = imgObject.map(item=>{ 
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}"> 
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>`
+    }).join("");
+
+    galaryList.insertAdjacentHTML('afterbegin', renderPhoto);
+
+}
+
+renderGalery(galleryItems);
+
+
+
+
+   const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+const getTime = (currentTime) =>{
+    localStorage.setItem("videoplayer-current-time", currentTime.seconds);
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+player.on('timeupdate', throttle(getTime, 1000));
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+ });
