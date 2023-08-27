67baa4b5790c0c16ff5b0f67cf2ef61b88074fde
Midomik <krastikrabbs9234@gmail.com>
2023-08-27 16:01:20 +0300

    comm 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+
+const formEl = document.querySelector(".feedback-form");
+const inputEl = formEl.querySelector("input");
+const textareaEl = formEl.querySelector("textarea");
+const objectInfo={};
+
+
+
+
+const writeFeedbaack = () => {
+    objectInfo.email=inputEl.value;
+    objectInfo.message=textareaEl.value;
+    // console.log(objectInfo);
+    localStorage.setItem("feedback-form-state", JSON.stringify(objectInfo))
+}
+ 
+const fillField = (lS) =>{
+   const localInfo = JSON.parse(lS.getItem("feedback-form-state"));
+        if(localInfo.email.trim()){
+            inputEl.value=localInfo.email;
+            // console.log("input ok")
+        } 
+        if(localInfo.message.trim()){
+            textareaEl.value=localInfo.message;
+            // console.log("area ok");
+        } 
+}
+
+const formSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    e.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+
+fillField(localStorage);
+
+formEl.addEventListener("input", throttle(writeFeedbaack,500));
+formEl.addEventListener("submit", formSubmit);
